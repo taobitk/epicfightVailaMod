02c872515687f6a42a42839435111c6ec502a900
taobitk <8hungvip8@gmail.com>
2026-07-08 20:00:27 +0700

    feat(kubejs/config): update recipes and sync mod configurations

--- kubejs/server_scripts/recipes/additions/minecraft/crafting/others/arrow_recipe.js
+++ kubejs/server_scripts/recipes/additions/minecraft/crafting/others/arrow_recipe.js
@@ -1,14 +1,14 @@
 ServerEvents.recipes(event => {
     // Thêm công thức chế tạo mũi tên mới (Cố định vị trí):
-    // Sắt (Iron Ingot) ở trên
+    // Đá lửa (Flint) ở trên
     // Gậy (Stick) ở dưới
     // -> 4 Mũi tên (Arrow)
     event.shaped('4x minecraft:arrow', [
-        ' I ',
+        ' F ',
         ' S ',
         '   '
     ], {
-        I: 'minecraft:iron_ingot',
+        F: 'minecraft:flint',
         S: 'minecraft:stick'
     })
 })
--- kubejs/server_scripts/recipes/additions/petting/add_petting.js
+++ kubejs/server_scripts/recipes/additions/petting/add_petting.js
@@ -1,5 +1,5 @@
 // Tên file: kubejs/server_scripts/recipes/additions/petting/add_petting.js
-// Mục đích: Thêm công thức chế tạo mới dạng bàn chế tạo (shaped) cho Golden Wheat.
+// Mục đích: Thêm công thức chế tạo mới dạng bàn chế tạo (shaped) cho Golden Wheat và Pet Bed.
 
 ServerEvents.recipes(event => {
     event.shaped('petting:golden_wheat', [
@@ -10,5 +10,14 @@ ServerEvents.recipes(event => {
         N: 'minecraft:netherite_block',
         G: 'kubejs:nidg',
         W: 'minecraft:wheat'
+    });
+
+    event.shaped('petting:pet_bed', [
+        'PPP',
+        'PBP',
+        'PPP'
+    ], {
+        P: '#minecraft:planks',
+        B: '#minecraft:beds'
     });
 });
