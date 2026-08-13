25adab14e072298e865fc86ff5006e8379e9c2f0
taobitk <8hungvip8@gmail.com>
2026-08-14 00:28:11 +0700

    Remove sugar requirement from custom bread crafting recipe

--- kubejs/server_scripts/recipes/additions/minecraft/crafting/food/bread_recipe.js
+++ kubejs/server_scripts/recipes/additions/minecraft/crafting/food/bread_recipe.js
@@ -1,7 +1,6 @@
 ServerEvents.recipes(event => {
-    // Công thức shapeless (tự do vị trí): 3 Lúa Mì + 1 Đường -> 1 Bánh Mì
+    // Công thức shapeless (tự do vị trí): 3 Lúa Mì -> 1 Bánh Mì (Bỏ Đường)
     event.shapeless('minecraft:bread', [
-        '3x minecraft:wheat',
-        'minecraft:sugar'
+        '3x minecraft:wheat'
     ]).id('kubejs:bread_custom_shapeless')
 })
