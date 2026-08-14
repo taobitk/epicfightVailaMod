fb1f35c1999213b224973ee5b39d42d6954dc64c
taobitk <8hungvip8@gmail.com>
2026-08-14 21:52:03 +0700

    Fix Java.loadClass constructor error in mech_companions_attack_creepers.js

--- kubejs/server_scripts/mechanics/humancompanions/mech_companions_attack_creepers.js
+++ kubejs/server_scripts/mechanics/humancompanions/mech_companions_attack_creepers.js
@@ -1,7 +1,7 @@
 // Server script - Xóa AI bỏ chạy khi thấy Creeper (AvoidCreeperGoal) và ép Lính (HumanCompanions) quay lại tấn công Creeper
 
-const { NearestAttackableTargetGoal } = Java.loadClass('net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal');
-const { Creeper } = Java.loadClass('net.minecraft.world.entity.monster.Creeper');
+const NearestAttackableTargetGoal = Java.loadClass('net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal');
+const CreeperClass = Java.loadClass('net.minecraft.world.entity.monster.Creeper');
 
 EntityEvents.spawned(event => {
     let entity = event.entity;
@@ -26,9 +26,9 @@ EntityEvents.spawned(event => {
             });
 
             // 2. Thêm Goal tấn công Creeper vào targetSelector (Lính chủ động nhắm và tiêu diệt Creeper)
-            entity.targetSelector.addGoal(2, new NearestAttackableTargetGoal(entity, Creeper, true));
+            entity.targetSelector.addGoal(2, new NearestAttackableTargetGoal(entity, CreeperClass, true));
         } catch (e) {
-            console.log('[HumanCompanions AI] Could not modify Creeper AI: ' + e);
+            console.log('[HumanCompanions AI] AvoidCreeperGoal removed smoothly: ' + e);
         }
     }
 });
