2d94982393d8afc31cb103eee4b37ae9fe0ad330
taobitk <8hungvip8@gmail.com>
2026-08-14 21:45:26 +0700

    Add doFireTick false to default gamerules script to prevent fire spread

--- kubejs/server_scripts/mechanics/minecraft/mech_default_gamerules.js
+++ kubejs/server_scripts/mechanics/minecraft/mech_default_gamerules.js
@@ -1,8 +1,12 @@
-// Tên file: kubejs/server_scripts/mechanics/mech_default_gamerules.js
+// Tên file: kubejs/server_scripts/mechanics/minecraft/mech_default_gamerules.js
 // Mục đích: Tự động cấu hình các luật game (gamerules) mặc định khi thế giới/server được tải lên.
 
 ServerEvents.loaded(event => {
     // Tự động bật mobGriefing thành true
     event.server.runCommandSilent('gamerule mobGriefing true');
-    console.info('[Gamerules] Đã tự động kích hoạt luật game: mobGriefing = true');
+    
+    // Tự động tắt cháy lan (Tắt doFireTick để lửa không lan ra nhà cửa/cây cối)
+    event.server.runCommandSilent('gamerule doFireTick false');
+    
+    console.info('[Gamerules] Đã tự động kích hoạt luật game: mobGriefing = true, doFireTick = false');
 });
